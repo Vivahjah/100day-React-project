3ce575880705bf7dbe326c1e8bbaf7e7b53a57bd
Vivahjah <mengwa44@yahoo.com>
2022-09-07 03:34:19 +0100

    debugging

--- crypto-react-app/src/App.js
+++ crypto-react-app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import CoinPage from "./pages/CoinPage";	
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+    <Routes>
+
+      <Route path="/" exact component={Homepage} />
+      <Route path="/coins/:id" exact component={CoinPage} />
+  
+    
+    </Routes>
+
+    </BrowserRouter>
+   
+  )
 }
 
 export default App;
