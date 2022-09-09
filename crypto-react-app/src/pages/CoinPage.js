94c47191b454dfb8cdbae94b25c3192b662d638a
Vivahjah <mengwa44@yahoo.com>
2022-09-09 01:25:57 +0100

    added a quiz app

--- crypto-react-app/src/pages/CoinPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const CoinPage = () => {
-  return <div>
-    <h4>CoinPage</h4>
-  </div>;
-};
-
-export default CoinPage;
--- crypto-react-app/src/pages/Homepage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Homepage = () => {
-  return <div>
-    <h4>Homepage</h4>
-  </div>;
-};
-
-export default Homepage;
