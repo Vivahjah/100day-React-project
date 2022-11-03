b22e67c0d0b3a6c4f8956cde6fda770c3965a89f
Mengwa Ugo <mengwa44@yahoo.com>
2022-11-03 13:37:48 +0100

    started a drag and drop project

---
+++ drag-and-drop/src/components/drop-file-input/DropFileInput.jsx
@@ -0,0 +1,22 @@
+import React, { useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import './drop-file-input.css';
+
+import { ImageConfig } from '../../config/ImageConfig'; 
+import uploadImg from '../../assets/cloud-upload-regular-240.png';
+
+const DropFileInput = props => {
+
+   
+
+    return (
+        <>
+            <h1>hello</h1>
+        </>
+    );
+}
+
+
+
+export default DropFileInput;
